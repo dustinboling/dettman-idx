b9ff77e58fb553daab6846871faa588cd9839a99
Dustin Boling <dustin@dustinboling.com>
2014-07-30 01:18:19 -0700

    commit all for initial push to github

--- app/controllers/residential_properties_controller.rb
+++ app/controllers/residential_properties_controller.rb
@@ -2,8 +2,14 @@ class ResidentialPropertiesController < ApplicationController
 
   def index
     @search = ResidentialProperty.search(params[:q])
-    @residential_properties = @search.result
+    
     @search.build_condition
+    @residential_properties = @search.result
+    
+    respond_to do |format|
+		format.html
+		format.json { render :json => @residential_properties }
+	end
   end
   
 end
